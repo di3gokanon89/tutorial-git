17e5807f651200364a09ae3e65f42fd4372de82a
di3gokanon89 <diego.paniagua.kanon89@gmail.com>
2020-12-10 16:59:42 -0600

    Version inicial del proyecto con git

---
+++ src/main/java/com/devpredator/tutorial_git/App.java
@@ -0,0 +1,13 @@
+package com.devpredator.tutorial_git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
