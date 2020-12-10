890f3eb86aa811bc07b4a48274d2de0616bf7820
di3gokanon89 <diego.paniagua.kanon89@gmail.com>
2020-12-10 17:07:43 -0600

    Nombre del Proyecto: tutorial-git
    Descripción: Se integra el mensaje para la versión 1.1 del tutorial de
    git.

--- src/main/java/com/devpredator/tutorial_git/App.java
+++ src/main/java/com/devpredator/tutorial_git/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Versión 1.1 del tutorial de git" );
     }
 }
